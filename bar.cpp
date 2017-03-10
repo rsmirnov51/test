baa1e5491eb33955a7d4bbf65f0417e0479a4758
rsmirnov51 <roman-smr@mail.ru>
2017-03-10 12:38:59 +0400

    Update bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -1,3 +1,14 @@
 #include <catch.hpp>
+#include <iostream>
+
+using namespace std;
+
+int stepen3(int N)
+{
+  int res = 1;
+  for (int i = 0; i < N; i++)  res *= 3;
+  return res;
+}
+
 TEST_CASE("Stepen is comp", [stepen]){
 }

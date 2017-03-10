22ec523527b5481fad7bdb59ea9fdd0088fc7a23
rsmirnov51 <roman-smr@mail.ru>
2017-03-10 12:12:58 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,13 @@
 #define CATH_CONFIG_MAIN
 #include "catch.cpp"
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+    int n = 0;
+    cout << "Enter n" << endl;
+    cin >> n;
+    cout << "2^" << n << " = " << stepen(n) << endl;
+}

4cb1a018e0e62d35dc4a0bbc311461ca49dc557f
rsmirnov51 <roman-smr@mail.ru>
2017-03-10 12:33:25 +0400

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,3 +1,11 @@
 #include <catch.hpp>
-TEST_CASE(){
+
+int stepen(int N)
+{
+  int res = 1;
+  for (int i = 0; i < N; i++)  res *= 2;
+  return res;
+}
+
+TEST_CASE("Stepen function complete", [stepen]){
 }

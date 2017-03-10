f332cf7d87d3a45460905ebd13db7eb990b78980
rsmirnov51 <roman-smr@mail.ru>
2017-03-10 13:30:42 +0400

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,18 +1,13 @@
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "iostream"
 
 using namespace std;
 
-int stepen2(int N)
-{
-  int res = 1;
-  for (int i = 0; i < N; i++)  res *= 2;
-  return res;
+unsigned int Factorial( unsigned int number );
+
+TEST_CASE("Stepen function complete", "[foo]"){
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
 }
 
-TEST_CASE("Stepen function complete", "[stepen2]"){
-    REQUIRE( stepen2(1) == 2 );
-    REQUIRE( stepen2(2) == 4 );
-    REQUIRE( stepen2(3) == 8 );
-    REQUIRE( stepen2(10) == 1024 );
-}

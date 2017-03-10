cc70797dcad887d159c748af7b9d17a3c4ec5a6a
rsmirnov51 <roman-smr@mail.ru>
2017-03-10 12:34:26 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@
 
 using namespace std;
 
-int stepen(int N)
+int stepen2(int N)
 {
   int res = 1;
   for (int i = 0; i < N; i++)  res *= 2;
+  return res;
+}
+
+int stepen3(int N)
+{
+  int res = 1;
+  for (int i = 0; i < N; i++)  res *= 3;
   return res;
 }
 
@@ -17,5 +24,6 @@ void main()
     int n = 0;
     cout << "Enter n" << endl;
     cin >> n;
-    cout << "2^" << n << " = " << stepen(n) << endl;
+    cout << "2^" << n << " = " << stepen2(n) << endl;
+    cout << "3^" << n << " = " << stepen3(n) << endl;
 }
